51b30fca9d0f0a0ba4682a500713cc7d003cbfd1
emiandd <emilandresaravena@icloud.com>
2022-08-28 21:27:00 -0400

    components: NavBar added for Mobile, Tablet and Desktop

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import s from './NavBar.module.scss';
+import { ReactComponent as SearchIcon } from '../../assets/icons/search-icon.svg'
+
+export default function NavBar() {
+	return (
+		<nav className={s.nav}>
+			<a href="/"><div className={s.nav__logo}></div></a>
+			<form className={s.nav__input} action="">
+				<input type="text" placeholder="Buscar..." /><button><SearchIcon /></button>
+			</form>
+			<div className={s.nav__burgerMenu}></div>
+		</nav>
+	)
+}
